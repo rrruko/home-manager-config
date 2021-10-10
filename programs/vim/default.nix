64f59c0c353a8af2da9e3c8228cc6c3ec8d4a999
rrruko <rukokarasu@gmail.com>
2021-10-10 12:24:12 -0700

    add sonokai colorscheme

--- programs/vim/default.nix
+++ programs/vim/default.nix
@@ -2,7 +2,21 @@
 {
   programs.vim = {
     enable = true;
+    plugins = with pkgs.vimPlugins; [ sonokai ];
     extraConfig = ''
+      " calling packadd! to silence startup error messages here, see:
+      " https://github.com/sainnhe/sonokai/blob/81e19fc3db1af37ec5acf2d7cb11b97ae48c34b6/doc/sonokai.txt#L409a
+      " > You'll need to add `packadd! sonokai` before applying this color scheme.
+      " > Check [sainnhe/gruvbox-material#60](https://github.com/sainnhe/gruvbox-material/issues/60)
+      packadd! sonokai
+
+      if has('termguicolors')
+        set termguicolors
+      endif
+      let g:sonokai_style = 'andromeda'
+      let g:sonokai_enable_italic = 1
+      colorscheme sonokai
+
       set number
       set linebreak
       set textwidth=80
