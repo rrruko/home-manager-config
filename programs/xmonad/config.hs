d92e3134bcf3a7d4598e04ec0ceb810c5b54f780
rrruko <rukokarasu@gmail.com>
2021-10-10 13:16:02 -0700

    add volume control keys

--- programs/xmonad/config.hs
+++ programs/xmonad/config.hs
@@ -1,5 +1,10 @@
+-- You can edit this by entering a nix shell:
+-- $ nix-shell -p ghcid "haskellPackages.ghcWithPackages (p: [ p.xmonad
+--   p.xmonad-contrib p.containers ])"
+
 import XMonad
 import XMonad.Layout.Spacing
+import qualified Data.Map as Map
 
 main :: IO ()
 main = do
@@ -7,4 +12,16 @@ main = do
     { borderWidth = 0
     , layoutHook = spacingRaw True (Border 0 2 2 2) True (Border 2 2 2 2) True $ layoutHook def
     , terminal = "kitty"
+    , keys = myKeys <> keys def
     }
+
+myKeys :: XConfig l -> Map.Map (ButtonMask, KeySym) (X ())
+myKeys XConfig { XMonad.modMask = modm } = Map.fromList
+  [ ((0, audioLowerVolume), spawn "amixer -q sset Master 5%-")
+  , ((0, audioRaiseVolume), spawn "amixer -q sset Master 5%+")
+  , ((0, audioMute), spawn "amixer set Master toggle")
+  ]
+  where
+    audioLowerVolume = 0x1008ff11
+    audioRaiseVolume = 0x1008ff13
+    audioMute = 0x1008ff12
