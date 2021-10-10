4deabf2fc5b5ba5696c3af1ede1a315ec6b89e0e
rrruko <rukokarasu@gmail.com>
2021-10-10 12:41:02 -0700

    remove extra whitespace

--- programs/vim/default.nix
+++ programs/vim/default.nix
@@ -24,19 +24,19 @@
       set linebreak
       set textwidth=80
       set showmatch
-       
+
       set hlsearch
       set incsearch
-       
+
       set autoindent
       set expandtab
       set shiftwidth=2
       set smartindent
       set smarttab
       set softtabstop=2
-       
+
       set ruler
-       
+
       set undolevels=1000
       set backspace=indent,eol,start
     '';
